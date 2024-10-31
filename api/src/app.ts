65c3cc4114e4d41e65ffc992de1ec9fdab919a77
zzeus-lab <eliseuwpcampos@gmail.com>
2024-10-30 22:42:23 -0300

    fix(cors)

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,9 @@
 import express, { Application } from "express";
 import movieRoutes from "./routes/movieRoutes";
 import dotenv from "dotenv";
-import "./config/database"; // Initialize database connection
-import "./cron/syncMoviesJob"; // Start the cron job for syncing movies
+import "./config/database";
+import "./cron/syncMoviesJob";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,16 @@ const app: Application = express();
 
 // Middleware for parsing JSON
 app.use(express.json());
+
+// Configure CORS options
+const corsOptions = {
+  origin: "http://localhost:3001", // Frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow cookies or auth headers in CORS requests if needed
+};
+
+// Apply CORS middleware to all routes
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/movies", movieRoutes);
