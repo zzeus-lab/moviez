ee53de026ba280a980d84d2dbd3eacbaeab3ec8f
zzeus-lab <eliseuwpcampos@gmail.com>
2024-10-31 03:02:06 -0300

    fix(webapp): app name

--- webapp/src/app/components/Navbar.tsx
+++ webapp/src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar({
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <h1 className="text-2xl font-bold text-blue-600">Movies App</h1>
+        <h1 className="text-2xl font-bold text-blue-600">Moviez</h1>
         <button
           onClick={onSync}
           className={`px-4 py-2 rounded-lg ${
